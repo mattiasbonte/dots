08693ab0e12ce292c520fa0c16984c8d798b52d1
Mattias B. <info@mattiasbonte.dev>
2025-05-18 14:21:15 +0200

    feat(arch): improve post install script

--- arch/post-install.sh
+++ arch/post-install.sh
@@ -1,64 +1,27 @@
-# Helpers
-i() {
-    local pm="$1"
-    shift
+# FN
+paci() { [ -z "$1" ] && return 0 || sudo pacman -S --needed "$@"; }
+pari() { [ -z "$1" ] && return 0 || paru -S --needed "$@"; }
 
-    if [ -z "$1" ]; then
-        echo "POST INSTALL ERROR: No package specified."
-        return 1
-    fi
-
-    if [ "$pm" = "pacman" ]; then
-        for pkg in "$@"; do
-            echo "Checking package: $pkg"
-            if ! pacman -Qi "$pkg" &>/dev/null; then
-                echo "Installing $pkg with pacman..."
-                sudo pacman -S --needed "$pkg" || {
-                    echo "Failed to install $pkg"
-                    return 1
-                }
-                echo "$pkg installed successfully"
-            else
-                echo "$pkg is already installed"
-            fi
-        done
-    elif [ "$pm" = "paru" ]; then
-        for pkg in "$@"; do
-            echo "Checking package: $pkg"
-            if ! paru -Qi "$pkg" &>/dev/null; then
-                echo "Installing $pkg with paru..."
-                paru -S "$pkg" || {
-                    echo "Failed to install $pkg"
-                    return 1
-                }
-                echo "$pkg installed successfully"
-            else
-                echo "$pkg is already installed"
-            fi
-        done
-    else
-        echo "POST INSTALL ERROR: Invalid package manager specified. Use 'pacman' or 'paru'."
-        return 1
-    fi
-}
-
-# Paru
+# AUR
 sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-# Shell
-i pacman zsh
-i pacman zsh-completions
+# ZSH
+paci zsh
+paci zsh-completions
 sudo chsh -s $(which zsh)
 
-# Yazi
-i pacman yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick
-paru resvg
+# FM
+paci yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick
+pari resvg
 
-# Git
-i pacman lazygit github-cli
+# GIT
+paci lazygit github-cli
 
-# Game
-i pacman pacman-contrib
+# PLAY
+paci pacman-contrib
+
+# WEB
+pari zen-browser-bin
