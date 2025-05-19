52fbc61f367d81d374375801021996ee4d96ff15
Mattias B. <info@mattiasbonte.dev>
2025-05-19 15:29:46 +0200

    feat(arch): add post init

--- arch/post-init.sh
+++ arch/post-init.sh
@@ -4,5 +4,9 @@
 # @note only after the init and github ssh key have been setup
 # --
 
+export BW_SESSION=$(bw unlock --raw)
+
 # CHEZMOI
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:mattiasbonte/dotfiles.git
+
+unset BW_SESSION
