ba51e474a204c80ae27f1869075a9eb5b200dfcd
Mattias B. <info@mattiasbonte.dev>
2025-05-25 12:41:31 +0200

    feat(arch): move z to post chezmoi steps

--- arch/post-init.sh
+++ arch/post-init.sh
@@ -3,17 +3,25 @@
 # @note only after the init and github ssh key have been setup
 # --
 
-# Checks
-if ! command -v gh &> /dev/null; then
-  echo "Install github-cli first: 'sudo pacman -S gh'"
-  exit 1
+# Pre Chezmoi Setup
+if ! command -v gh &>/dev/null; then
+    echo "Install github-cli first: 'sudo pacman -S gh'"
+    exit 1
 fi
-if ! command -v bw &> /dev/null; then
-  echo "Install bitwarden-cli first: 'sudo pacman -S bitwarden-cli'"
-  exit 1
+if ! command -v bw &>/dev/null; then
+    echo "Install bitwarden-cli first: 'sudo pacman -S bitwarden-cli'"
+    exit 1
 fi
 gum confirm --default=false "Have you authorized github-cli (so that your github ssh key is set up)?" || gh auth login
 gum confirm --default=false "Have you authorized bitwarden-cli (needed for chezmoi)?" || bw login
+
+# CHEZMOI
+export BW_SESSION=$(bw unlock --raw)
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:mattiasbonte/dotfiles.git
+unset BW_SESSION
+
+# Post Chezmoi Setup
+source ~/.zshrc
 
 # Shortcuts
 z ~/DOTS
@@ -23,9 +31,5 @@ z ~/DEV/GO/breaks
 z ~/.local/share/chezmoi
 z ~/DEV/PROFESSION/WINTRO/wintro-mono/app
 
-# CHEZMOI
-export BW_SESSION=$(bw unlock --raw)
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:mattiasbonte/dotfiles.git
-unset BW_SESSION
-
+# Reboot
 gum confirm --default=false "Reboot now?" && reboot || "Reboot later"
